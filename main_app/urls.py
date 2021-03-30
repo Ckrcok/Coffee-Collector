0e145a20601d9afc721c81b0783dcd1f24de3bcc
Dor <dorzairi@ymail.com>
2021-03-30 17:29:13 -0400

    Changed -  the base , urls, views Added - Details, Confirm delete, Form

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('coffee/', views.coffee_index, name='coffee')
+    path('coffee/', views.coffee_index, name='index'),
+    path('coffee/<int:cat_id>/', views.coffees_detail, name='detail'),
+    path('coffee/create/', views.CoffeeCreate.as_view(), name='coffee_create'),
+    path('coffee/<int:pk>/update/', views.CoffeeUpdate.as_view(), name='coffee_update'),
+    path('coffee/<int:pk>/delete/', views.CoffeeDelete.as_view(), name='coffee_delete'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirct
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from .models import Coffee
 
 
 
 
-class Coffee:
-  def __init__(self, name, breed, description):
-    self.name = name
-    self.type = type
-    self.description = description
-
-coffees = [
-  Coffee('Arabica Coffee', 'Arabica', ' is the most common (and certainly most heavily marketed) type of coffee in North America. That’s because it actually has a sweeter, more delicate flavor and the coffee itself tends to be less acidic.'),
-  Coffee('Robusta Coffee', 'Robusta', 'Robusta coffee beans are second on the list and the most popular in Europe, the Middle East and Africa. Its name does this bean justice, as it is known for its strong and often harsh flavor profile. Robusta coffees have extremely high levels of caffeine, which makes the plant far more resilient than the Arabica species.'),
-  Coffee('Liberica Coffee', 'Liberica', 'Liberica coffee beans are a rare treat. They’re grown in very specific climates with production being far too scarce for farmers to scale their operations to truly satisfy a global marketplace. Even still, the beans are considered a pleasant surprise. Many who’ve tried the coffee liken the aroma to fruit and flowers and describe the flavor as having a somewhat “woody” taste.')
-]
+# coffees = [
+#   Coffee('Arabica Coffee', 'Arabica', ' is the most common (and certainly most heavily marketed) type of coffee in North America. That’s because it actually has a sweeter, more delicate flavor and the coffee itself tends to be less acidic.'),
+#   Coffee('Robusta Coffee', 'Robusta', 'Robusta coffee beans are second on the list and the most popular in Europe, the Middle East and Africa. Its name does this bean justice, as it is known for its strong and often harsh flavor profile. Robusta coffees have extremely high levels of caffeine, which makes the plant far more resilient than the Arabica species.'),
+#   Coffee('Liberica Coffee', 'Liberica', 'Liberica coffee beans are a rare treat. They’re grown in very specific climates with production being far too scarce for farmers to scale their operations to truly satisfy a global marketplace. Even still, the beans are considered a pleasant surprise. Many who’ve tried the coffee liken the aroma to fruit and flowers and describe the flavor as having a somewhat “woody” taste.')
+# ]
 
 
 # Create your views here.
+class CoffeeCreate(CreateView):
+    model = Coffee
+    fields = '__all__'
+
+class CoffeeUpdate(UpdateView):
+    model = Coffee
+    fields = ['name', 'description', 'type']
+
+class CoffeeDelete(DeleteView):
+    model = Coffee
+    success_url ='/coffee/'
+
 def home(request):
     return render(request, 'base.html')
 
@@ -27,5 +34,9 @@ def about(request):
 
 
 def coffee_index(request):
-    coffees = Coffee.object.all()
+    coffees = Coffee.objects.all()
     return render(request,'coffees/index.html', {'coffees':coffees})
+
+def coffees_detail(request, cat_id):
+  coffee = Coffee.objects.get(id=cat_id)
+  return render(request, 'coffee/detail.html', { 'coffee': coffee })
