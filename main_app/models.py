728d085419720d69b193f6b7e3a485e90f759c4a
Dor <dorzairi@ymail.com>
2021-03-30 22:30:52 -0400

    Small fixed for models, about & home

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Coffee(models.Model):
     name = models.CharField(max_length=(100))
     type = models.CharField(max_length=(100))
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=400)
 
     def __str__(self):
         return self.name
