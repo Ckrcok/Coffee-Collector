2cd5c89d3345c90a0dc181b9d4b397030f245115
Dor <dorzairi@ymail.com>
2021-03-31 21:49:07 -0400

    Updated the Models.py

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,17 @@ SUGARS =(
 )
 
 # Create your models here.
+class Flavor(models.Model):
+    name = models.CharField(max_length = 50)
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('falvors_detail', kwargs={'pk':self.id})
+
+
 class Coffee(models.Model):
     name = models.CharField(max_length=(100))
     type = models.CharField(max_length=(100))
@@ -24,3 +35,9 @@ class Sugar(models.Model):
     type = models.CharField(max_length=(8), choices=(SUGARS), default=SUGARS[0][0])
     amount = models.IntegerField()
     coffee = models.ForeignKey(Coffee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_suger_display()} on{self.date}"
+
+        class Meta:
+            ordering = ['-date']
