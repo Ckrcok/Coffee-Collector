795831d00a12bbde36f94abcb772e58923303f8c
Dor <dorzairi@ymail.com>
2021-03-31 21:50:58 -0400

    Added flavor_confirm_delete.html

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Coffee, Sugar
+from .models import Coffee, Sugar, Flavor
 
 # Register your models here.
 admin.site.register(Coffee)
 admin.site.register(Sugar)
+admin.site.register(Flavor)
